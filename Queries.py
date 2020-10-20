e604774a4a9dbbdf583e83a3d80508610e8bc89f
Ragnhild <ragnhildk98@gmail.com>
2020-10-20 20:58:15 +0200

    q2 & 3

--- Queries.py
+++ Queries.py
@@ -9,20 +9,43 @@ class Queries:
         self.connection = DbConnector()
         self.client = self.connection.client
         self.db = self.connection.db 
+        self.activity = self.db['Activity']
+        self.user = self.db['User']
+        self.trackpoint = self.db['TrackPoint']
 
-    def query1(self, col_name):
-        collection = self.db[col_name]
-        users = collection.count_documents({})
+
+    def query1(self, collection):
+        count = collection.count_documents({})
         #count_activities = db.Activity.find({}).count()
-        print(users)
+        print(count)
+
+    def query2(self): 
+        activity = self.activity.count_documents({})
+        user = self.user.count_documents({})
+        avg = activity / user
+        print(avg)
+
+    def query3(self):
+        query = self.activity.aggregate([
+            {'$group': {'_id': '$user_id', 
+                'count': { '$sum': 1}
+                }
+            }, 
+            {'$sort': {'count':-1}},
+            {'$limit': 20}
+        ])
+        for item in query:
+            pprint(item)
 
     def main(self):
-        print("User:")
-        self.query1(col_name='User')
-        print("Activity:")
-        self.query1(col_name='Activity')
-        print("TrackPoint:")
-        self.query1(col_name='TrackPoint')
+        #print("User:")
+        #self.query1(self.user)
+        #print("Activity:")
+        #self.query1(self.activity)
+        #print("TrackPoint:")
+        #self.query1(self.trackpoint)
+        #self.query2()
+        #self.query3()
 
 if __name__ == "__main__":
     try:
