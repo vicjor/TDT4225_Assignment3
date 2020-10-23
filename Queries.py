609e0ebb276931ca6fd2be7b868ce098ca251713
Petter Norsted Kildebo <petter_nk@hotmail.com>
2020-10-23 07:51:18 +0200

    q10, q11 good. q8, q9 no real output

--- Queries.py
+++ Queries.py
@@ -1,6 +1,8 @@
+from unittest.case import _id
+
 from DbConnector import DbConnector
 from haversine import haversine
-from pprint import pprint 
+from pprint import pprint
 
 
 class Queries:
@@ -58,19 +60,144 @@ class Queries:
 
     #def query6b(self):
 
+    def query8(self):
+        query = self.activity.aggregate([
+            {
+                '$match': {
+                    'trackpoints.altitude': {'$ne': '-777'}
+                }
+            },
+            {
+                '$lookup': {
+                    'from': 'TrackPoint',
+                    'localField': '_id',
+                    'foreignField': 'activity_id',
+                    'as': 'trackpoints'
+                }
+            }, {
+                '$project': {
+                    'trackpoints.altitude': 1,
+                    'user_id': 1
+                }
+            }])
+
+        totAltitude = {}
+        for item in query:
+            gainedHight = item['trackpoints']
+            res = 0
+
+            for i in range(len(gainedHight) - 1):
+                if gainedHight[i]['altitude'] == '-777':
+                    continue
+
+                if float(gainedHight[i]['altitude']) < float(gainedHight[i + 1]['altitude']):
+                    res += (float(gainedHight[i + 1]['altitude']) - float(gainedHight[i]['altitude'])) * 0.3048
+
+            totAltitude[item['user_id']] = totAltitude.get(item['user_id'], 0) + res
+
+        print('Query 8: Top 20 users that gained most altitude: ')
+
+        for user in totAltitude:
+            pprint(user)
+
+    def query9(self):
+        query = self.activity.aggregate([
+            {
+                '$lookup': {
+                    'from': 'TrackPoint',
+                    'localField': '_id',
+                    'foreignField': 'activity_id',
+                    'as': 'trackpoints'
+                }
+            },
+            {
+                '$project': {
+                    'trackpoints.date_days': 1,
+                    'user_id': 1
+                }
+            }])
+
+        totInvalidActivities = {}
+
+        for item in query:
+            invalidActivity = item['trackpoints']
+
+            for i in range(len(invalidActivity) - 1):
+                if float(invalidActivity[i + 1]['date_days']) - float(invalidActivity[i]['date_days']) >= 0.00347222:
+                    totInvalidActivities[item['user_id']] = totInvalidActivities.get(item['user_id'], 0) + 1
+                    break
+
+        print('Query 9: Total number of invalid activities: ')        
+
+        for user in totInvalidActivities:
+            pprint(user)
+
+    def query10(self):
+        query = self.trackpoint.aggregate([
+            {
+                '$match': {'$expr': {'$and': [
+                    {'$eq': [{'$round': ['$lat', 3]}, 39.916]},
+                    {'$eq': [{'$round': ['$lon', 3]}, 116.397]}
+                    ]
+                    }
+                }
+            },
+            {
+                '$lookup': {
+                    'from': 'Activity',
+                    'localField': 'activity_id',
+                    'foreignField': '_id',
+                    'as': 'forbiddenCity'
+                }
+            },
+            {'$group': {'_id': '$forbiddenCity.user_id'},},
+
+            {'$project': {'forbiddenCity.user_id': 1}}
+        ])
+
+        print("Query 10: Users who have tracked an activity in the forbidden city of Beijing")
+        for item in query:
+            pprint(item)
+
+    def query11(self):
+
+        query = self.activity.aggregate([
+            {'$match': {'transportation_mode': {'$exists': True,'$ne': 'null'}}},
+
+            {'$group': {'_id': {'category': '$transportation_mode','user': '$user_id'},
+                        'count': {'$sum': 1},}},
+
+            {'$sort': {'count': -1,'_id.category': -1}},
+
+            {'$group': {'_id': '$_id.user', 'count': {'$first': '$count'},'category': {'$first': '$_id.category'}}},
+
+            {'$project': {'_id': '$_id', 'category': '$category','count': '$count',}},
+
+            {'$sort': {'_id': 1}}
+        ])
+        print('Query 11: Most used transportation mode per user')
+        print('Format: [user_id, transportation_mode, times used]')
+
+        for item in query:
+            pprint(item)
+
     def main(self):
-        #print("User:")
+        #print('User:')
         #self.query1(self.user)
-        #print("Activity:")
+        #print('Activity:')
         #self.query1(self.activity)
-        #print("TrackPoint:")
+        #print('TrackPoint:')
         #self.query1(self.trackpoint)
         #self.query2()
         #self.query3()
         #self.query4()
         #self.query5()
+        self.query8()
+        self.query9()
+        #self.query10()
+        #self.query11()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     try:
         qo = Queries()
         qo.main()
@@ -85,7 +212,7 @@ def main():
         program = Queries()
         program.query1()
     except Exception as e:
-        print("ERROR: Failed to use database:", e)
+        print('ERROR: Failed to use database:', e)
     finally:
         if program:
             program.connection.close_connection()
